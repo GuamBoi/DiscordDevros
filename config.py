ad28e6cc07156831cac8a977d60fc5e389741a3e
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-03-15 15:03:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # Fetch bot settings
 DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-COMMAND_PREFIX = os.getenv("COMMAND_PREFIX", "!")
+COMMAND_PREFIX = os.getenv("COMMAND_PREFIX", "?")
 
 # Fetch OpenWebUI API settings
 OPENWEBUI_API_URL = os.getenv("OPENWEBUI_API_URL")
