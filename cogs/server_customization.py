a212ebe6ca547afcb9da6a72664ac2e53582247c
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-03-22 18:59:58 -0400

    Update server_customization.py

--- cogs/server_customization.py
+++ cogs/server_customization.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 import os
 from utils.embed import create_embed
-import config  # Import config.py
+import config
 
 class ServerCustomization(commands.Cog):
     def __init__(self, bot):
