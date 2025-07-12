f1eccea279689cc8115be7631d10971c8b473f1e
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-07-11 20:10:10 -0400

    Update xp.py

--- cogs/xp.py
+++ cogs/xp.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from config import XP_PER_MESSAGE, XP_PER_REACTION, XP_PER_COMMAND
 from utils.economy import add_xp
+from utils.embed import create_embed
 
 class XP(commands.Cog):
     """Cog to award XP and handle level‑up rewards."""
@@ -11,45 +12,47 @@ class XP(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Ignore bot messages
         if message.author.bot:
             return
 
-        # Award XP for a regular message
         leveled, new_level = add_xp(message.author.name, XP_PER_MESSAGE)
         if leveled:
-            await message.channel.send(
-                f"🎉 {message.author.mention} just hit level **{new_level}** and earned **{new_level} Devros$**!"
+            embed = await create_embed(
+                title="Level Up! 🎉",
+                description=f"{message.author.mention}, you just hit level **{new_level}** and earned **{new_level} Devros$**!",
+                color=discord.Color.green()
             )
+            await message.channel.send(embed=embed)
 
-        # Ensure other commands still process
         await self.bot.process_commands(message)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        # Ignore bot reactions
         if user.bot:
             return
 
-        # Award XP for reacting
         leveled, new_level = add_xp(user.name, XP_PER_REACTION)
         if leveled:
-            await reaction.message.channel.send(
-                f"🎉 {user.mention} just hit level **{new_level}** and earned **{new_level} Devros$**!"
+            embed = await create_embed(
+                title="Level Up! 🎉",
+                description=f"{user.mention}, you just hit level **{new_level}** and earned **{new_level} Devros$**!",
+                color=discord.Color.green()
             )
+            await reaction.message.channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        # Ignore bot‑originated commands
         if ctx.author.bot:
             return
 
-        # Award XP for any Devros command use
         leveled, new_level = add_xp(ctx.author.name, XP_PER_COMMAND)
         if leveled:
-            await ctx.send(
-                f"🎉 {ctx.author.mention} just hit level **{new_level}** and earned **{new_level} Devros$**!"
+            embed = await create_embed(
+                title="Level Up! 🎉",
+                description=f"{ctx.author.mention}, you just hit level **{new_level}** and earned **{new_level} Devros$**!",
+                color=discord.Color.green()
             )
+            await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(XP(bot))
