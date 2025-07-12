5f95dc865b4eae8bd20a71e4c9dca2a3682983f8
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-07-11 23:44:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+# WARNING: This file contains default configuration values.
+#          Sensitive values should only be stored in .env!
+
 import os
 from dotenv import load_dotenv
 
@@ -15,19 +18,25 @@ BOT_VERSION = "YOUR_VERSION_NUMBER"  # The version of your bot
 COMMAND_PREFIX = "YOUR_PREFIX"       # Change value if you want different prefix.
 MODEL_NAME = "YOUR_LLM_MODEL_NAME"   # Set your model name here.
 
-# Server Economy Settings
-ECONOMY_FOLDER = "data/economy_data"         # Folder where economy files are saved
-CURRENCY_NAME = "CURRENCY_NAME"              # Name of your server's currency
-CURRENCY_SYMBOL = "CURRENCY_SYMBOL"          # Symbol of your server's currency
+## XP Values
 XP_PER_MESSAGE =  NUMBER_HERE                # XP gained from regular messages
 XP_PER_REACTION = NUMBER_HERE                # XP gained from reacting to messages
 XP_PER_COMMAND = NUMBER_HERE                 # XP gained from using Devros commands
+LEVEL_UP_REWARD_MULTIPLIER = NUMBER_HERE     # Default value Level Up reward (Value * LVL Earned)
+
+## Server Economy Settings
+### Currency Values
+ECONOMY_FOLDER = "data/economy_data"         # Folder where economy files are saved
+CURRENCY_NAME = "CURRENCY_NAME"              # Name of your server's currency
+CURRENCY_SYMBOL = "CURRENCY_SYMBOL"          # Symbol of your server's currency
 DEFAULT_CURRENCY_GIVE = NUMBER_HERE          # Default value adding currency
 DEFAULT_CURRENCY_TAKE = NUMBER_HERE          # Default value adding currency
 GAME_WIN = NUMBER_HERE                       # Game win currency value
 GAME_LOSE = NUMBER_HERE                      # Game lost currency value
 
-# Channel Specifications (Defined directly in config.py, not from .env)
+### Stock Values (COMING SOON!!)
+
+## Channel Specifications (Defined directly in config.py, not from .env)
 WORDLE_CHANNEL = WORDLE_CHANNEL_ID                             # Set your Wordle Game Channel
 INVITE_CHANNEL = INVITE_CHANNEL_ID                             # Set your Game Invite Channel ID
 WELCOME_CHANNEL = WELCOME_CHANNEL_ID                           # Set your Welcome Channel ID
