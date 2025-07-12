893accc6f811e54efd65263d7863887d29303cf0
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2025-07-12 00:42:52 -0400

    Update config.py

--- config.py
+++ config.py
@@ -13,34 +13,45 @@ OPENWEBUI_API_KEY = os.getenv("OPENWEBUI_API_KEY")     # Pulled from .env file
 OPENWEBUI_API_URL = os.getenv("OPENWEBUI_API_URL")     # Pulled from .env file
 
 # Non-sensitive settings (Additional bot settings that are okay to share)
-BOT_NAME = "YOUR_BOTS_NAME"          # The name of your bot
-BOT_VERSION = "YOUR_VERSION_NUMBER"  # The version of your bot
-COMMAND_PREFIX = "YOUR_PREFIX"       # Change value if you want different prefix.
-MODEL_NAME = "YOUR_LLM_MODEL_NAME"   # Set your model name here.
+COMMAND_PREFIX = "!"              # Change value if you want different prefix.
+MODEL_NAME = "devros-mini"        # Set your model name here.
+ECONOMY_FOLDER = "data/eco"       # Folder where economy files are saved
 
-## XP Values
-XP_PER_MESSAGE =  NUMBER_HERE                # XP gained from regular messages
-XP_PER_REACTION = NUMBER_HERE                # XP gained from reacting to messages
-XP_PER_COMMAND = NUMBER_HERE                 # XP gained from using Devros commands
-LEVEL_UP_REWARD_MULTIPLIER = NUMBER_HERE     # Default value Level Up reward (Value * LVL Earned)
+# Bot Info
+BOT_NAME = "Devros"               # The name of your bot
+BOT_VERSION = "2.1 Beta"          # The version of your bot
 
-## Server Economy Settings
-### Currency Values
-ECONOMY_FOLDER = "data/economy_data"         # Folder where economy files are saved
-CURRENCY_NAME = "CURRENCY_NAME"              # Name of your server's currency
-CURRENCY_SYMBOL = "CURRENCY_SYMBOL"          # Symbol of your server's currency
-DEFAULT_CURRENCY_GIVE = NUMBER_HERE          # Default value adding currency
-DEFAULT_CURRENCY_TAKE = NUMBER_HERE          # Default value adding currency
-GAME_WIN = NUMBER_HERE                       # Game win currency value
-GAME_LOSE = NUMBER_HERE                      # Game lost currency value
+# Role Info
+MODERATOR_ROLE_ID = 1035393475631394896      # Role ID for server mods
 
-### Stock Values (COMING SOON!!)
+# XP Values
+ENABLE_XP_SYSTEM = True           # Set to False to disable XP System
+SHOW_LEVEL_UP_MESSAGES = True     # Set to False to disable level up messages
+XP_NOTIFICATION_CHANNEL_ID = None  # Replace with your actual channel ID, or set to None to use current channel
+XP_PER_MESSAGE =  1               # XP gained from regular messages
+XP_PER_REACTION = 1               # XP gained from reacting to messages
+XP_PER_COMMAND = 5                # XP gained from using Devros commands
+LEVEL_UP_REWARD_MULTIPLIER = 100     # Default value Level Up reward (Value * LVL Earned)
 
-## Channel Specifications (Defined directly in config.py, not from .env)
-WORDLE_CHANNEL = WORDLE_CHANNEL_ID                             # Set your Wordle Game Channel
-INVITE_CHANNEL = INVITE_CHANNEL_ID                             # Set your Game Invite Channel ID
-WELCOME_CHANNEL = WELCOME_CHANNEL_ID                           # Set your Welcome Channel ID
-GOODBYE_CHANNEL = GOODBYE_CHANNEL_ID                           # Set your Goodbye Channel ID
-BETTING_CHANNEL = BETTING_CHANNEL_ID                           # Set your Betting Channel ID
-HELP_COMMAND_CHANNEL_CATEGORY = HELP_COMMAND_CATEGORY_ID       # Set your Help Commands Channel ID
-ROLLS_CHANNEL = BETTING_CHANNEL_ID                             # Set Roll Selection Channel ID
+# Server Economy Settings
+## Currency Values
+CURRENCY_NAME = "Devros Dolhairs"  # Name of your server's currency
+CURRENCY_SYMBOL = "Đ"
+DEFAULT_CURRENCY_GIVE = 100       # Default value adding currency (Also the amount players start with when their wallet is first generated)
+DEFAULT_CURRENCY_TAKE = 100       # Default value removing currency
+GAME_WIN = 50                     # Game won currency value
+GAME_LOSE = 25                    # Game lost currency value
+
+# Channel Specifications (Defined directly in config.py, not from .env)
+## Game Channels
+INVITE_CHANNEL = 1036762745527357450         # Set Game Invite Channel ID
+WORDLE_CHANNEL = 1209772800492179526         # Set Wordle Game Channel
+CONNECT4_CHANNEL = 1213909567965102111       # Set the Connect 4 Channel
+BATTLESHIP_CHANNEL = 1354106419435278336     # Set the Battleship Channel
+
+## Channels
+WELCOME_CHANNEL = 1036760459161911366        # Set Welcome Channel ID
+GOODBYE_CHANNEL = 1206374744719626361        # Set Goodbye Channel ID
+HELP_COMMAND_CHANNEL_CATEGORY = 1036929287346999326
+ROLLS_CHANNEL = 1036732354426843146          # Set Roll Selection Channel ID
+BETTING_CHANNEL = 1351377151676518483        # Set Betting Channel ID
