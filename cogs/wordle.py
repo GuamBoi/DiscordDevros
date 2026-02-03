2fdc5e77344af48be8068ab1bc92e2bbad1eb77b
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2026-02-02 21:58:31 -0700

    Update wordle.py

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 import json
 import random
-from config import GAME_WIN, GAME_LOSE, ECONOMY_FOLDER, WORDLE_CHANNEL
+from config import GAME_WIN, GAME_LOSE, ECONOMY_FOLDER, WORDLE_CHANNEL, CURRENCY_NAME
 from utils import economy
 from utils.embed import create_embed
 from utils.economy import user_key
@@ -92,14 +92,14 @@ class Wordle(commands.Cog):
             econ["wordle_streak"] = econ.get("wordle_streak", 0) + 1
             economy.save_economy(username, econ)
             economy.add_currency(username, GAME_WIN)
-            description += f"\n\nCongratulations {ctx.author.mention}! You won and earned **{GAME_WIN} {economy.get_currency_name()}**!"
+            description += f"\n\nCongratulations {ctx.author.mention}! You won and earned **{GAME_WIN} {CURRENCY_NAME}**!"
             del active_games[username]
         elif game["attempts"] >= MAX_ATTEMPTS:
             econ = economy.load_economy(username)
             econ["wordle_streak"] = 0
             economy.save_economy(username, econ)
             economy.remove_currency(username, GAME_LOSE)
-            description += f"\n\nGame Over! The correct word was **{answer}**. You lost **{GAME_LOSE} {economy.get_currency_name()}**."
+            description += f"\n\nGame Over! The correct word was **{answer}**. You lost **{GAME_LOSE} {CURRENCY_NAME}**."
             del active_games[username]
 
         embed = await create_embed("Wordle Game", description)
