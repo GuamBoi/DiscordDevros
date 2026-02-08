24a2cfd7c310665044f22a1343a7c29903f7c85c
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2026-02-08 16:23:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ OPENWEBUI_API_URL = os.getenv("OPENWEBUI_API_URL")     # Pulled from .env file
 # Non-sensitive settings (Additional bot settings that are okay to share)
 COMMAND_PREFIX = "!"              # Change value if you want different prefix.
 MODEL_NAME = "devros-mini"        # Set your model name here.
-ECONOMY_FOLDER = "data/ecoonomy"       # Folder where server members economy files are saved
+ECONOMY_FOLDER = "data/economy"       # Folder where server members economy files are saved
 
 # Bot Info
 BOT_NAME = "Devros"                           # The name of your bot
