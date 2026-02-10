f2bd41be5b2a348b692f01898a5556aed8caee2e
GuamBoi <68975905+GuamBoi@users.noreply.github.com>
2026-02-09 21:58:04 -0700

    Update server_customization.py

--- cogs/server_customization.py
+++ cogs/server_customization.py
@@ -4,7 +4,7 @@ import json
 import os
 from utils.embed import create_embed
 import config
-from utils.economy import load_economy  # removed dead imports
+from utils.economy import load_economy
 
 class ServerCustomization(commands.Cog):
     def __init__(self, bot):
